dd19bfd3a52b7eb6920b2613eb422ed17ceae945
Manish Gupta <mgupta.iitr@gmail.com>
2026-01-14 20:21:14 +0000

    GB200 lock scripts

--- nvgpu-utils/lock_clocks_gb200_1860MHz.sh
+++ nvgpu-utils/lock_clocks_gb200_1860MHz.sh
@@ -1,3 +1,3 @@
 sudo nvidia-persistenced --uvm-persistence-mode
-sudo nvidia-smi -i $1 -lgc $2,$2
+sudo nvidia-smi -i $1 -lgc 1860,1860
 sudo nvidia-smi -pl 1200 -i $1
--- nvgpu-utils/lock_clocks_gb200_1980MHz.sh
+++ nvgpu-utils/lock_clocks_gb200_1980MHz.sh
@@ -1,3 +1,3 @@
 sudo nvidia-persistenced --uvm-persistence-mode
-sudo nvidia-smi -i $1 -lgc $2,$2
+sudo nvidia-smi -i $1 -lgc 1980,1980
 sudo nvidia-smi -pl 1200 -i $1
